83b85e7acd47931bc2f9d2c5ca299e8aa869005a
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-09-18 18:15:07 -0300

    Implemented header

--- view/components/header.php
+++ view/components/header.php
@@ -1,4 +1,13 @@
-<header class="text-2xl font-bold text-center bg-slate-400 p-3 w-full flex flex-col gap-y-2">
-  <h1>Header</h1>
-  <p class="bg-red-500 font-bold">Obs.: só teste</p>
+<header class="bg-[#0D0D0D] text-white p-4 h-[100px] flex flex-wrap justify-center gap-y-3 fixed top-0 left-0 w-full items-center sm:justify-between md:px-8">
+  <span class="flex gap-x-2 text-3xl min-w-[320px] w-[100%] max-w-[420px]">
+    <h1 class="font-bold">Pet Shop</h1>
+    <i class="bi bi-heart-fill"></i>
+  </span>
+  <ul class="flex gap-x-4 min-w-[320px] w-[100%] max-w-[420px] sm:justify-end">
+    <li><a href="" class="text-[#57C7FF]">Home</a></li>
+    <li><a href="" class="text-[#57C7FF]">Sobre</a></li>
+    <li><a href="" class="text-[#57C7FF]">Produtos</a></li>
+    <li><a href="" class="text-[#57C7FF]">Formulário</a></li>
+  </ul>
+  <?php ?>
 </header>
