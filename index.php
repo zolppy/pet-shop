b1feb03f26b6ff999b305d8c460b120879804463
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-09-18 09:40:55 -0300

    Added navigation test

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     />
     <meta name="author" content="Gabriel Cavalcante e Rulian Cruz" />
     <title>Pet Shop</title>
-    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
       tailwind.config = {
@@ -27,14 +27,28 @@
     </script>
   </head>
   <body class="bg-slate-200 min-h-svh relative flex flex-col text-white">
-    <header class="text-2xl font-bold text-center bg-slate-400 p-3 w-full">
-      <h1>Header | Teste</h1>
+    <header class="text-2xl font-bold text-center bg-slate-400 p-3 w-full flex flex-col gap-y-2">
+      <h1>Header</h1>
+      <p class="bg-red-500 font-bold">Obs.: só teste</p>
+      <nav>
+        <ul class="flex gap-x-4 justify-center bg-black p-2">
+          <li class="underline">
+            <a href="view/about.php">Sobre</a>
+          </li>
+          <li class="underline">
+            <a href="view/list.php">Lista</a>
+          </li>
+          <li class="underline">
+            <a href="view/form.php">Formulário</a>
+          </li>
+        </ul>
+      </nav>
     </header>
     <main>
       <section
         class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
       >
-        <h2 class="text-2xl font-bold text-blue-900">Main</h2>
+        <h2 class="text-4xl font-bold text-blue-900">Main</h2>
       </section>
     </main>
     <footer class="absolute bottom-0 bg-slate-400 p-3 text-center w-full">
