a5d23c937b4a91d4be37fb1b804fc08fb5b59de4
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-09-18 09:18:34 -0300

    added view pages

---
+++ view/about.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="Encontre tudo o que seu pet precisa em nosso pet shop online! Oferecemos produtos de qualidade, como rações, brinquedos, acessórios e mais, com entrega rápida e preços acessíveis."
+    />
+    <meta
+      name="keywords"
+      content="Pet shop, produtos para pets, ração, acessórios para pets, brinquedos para animais, cuidados com animais, pet shop online, entrega rápida, loja de animais"
+    />
+    <meta name="author" content="Gabriel Cavalcante e Rulian Cruz" />
+    <title>Pet Shop</title>
+    <link rel="shortcut icon" href="../imagens/favicon.ico" type="image/x-icon" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {},
+        },
+        corePlugins: {
+          preflight: true,
+        },
+      };
+    </script>
+  </head>
+  <body></body>
+</html>
