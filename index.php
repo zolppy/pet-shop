4f1484499f66fc38ea1967df083ad1dfe1f5bea1
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-09-18 09:15:12 -0300

    Added meta tags "description", "author" e "keywords" to index.php

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="Encontre tudo o que seu pet precisa em nosso pet shop online! Oferecemos produtos de qualidade, como rações, brinquedos, acessórios e mais, com entrega rápida e preços acessíveis."
+    />
+    <meta
+      name="keywords"
+      content="Pet shop, produtos para pets, ração, acessórios para pets, brinquedos para animais, cuidados com animais, pet shop online, entrega rápida, loja de animais"
+    />
+    <meta name="author" content="Gabriel Cavalcante e Rulian Cruz" />
     <title>Pet Shop</title>
     <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon" />
     <script src="https://cdn.tailwindcss.com"></script>
