95202efcee2077bbbd6ee2b24dd4c5eb8b0e43e7
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-09-18 22:33:05 -0300

    Implemented hover transition on navigation items

--- view/components/header.php
+++ view/components/header.php
@@ -1,13 +1,13 @@
-<header class="bg-[#0D0D0D] text-white p-4 h-[100px] flex flex-wrap justify-center gap-y-3 fixed top-0 left-0 w-full items-center sm:justify-between md:px-8">
+<header class="bg-[#0D0D0D] text-white p-4 h-[100px] flex flex-wrap justify-center gap-y-3 fixed top-0 left-0 w-full items-center sm:justify-between sm:px-8">
   <span class="flex gap-x-2 text-3xl min-w-[320px] w-[100%] max-w-[420px]">
     <h1 class="font-bold">Pet Shop</h1>
     <i class="bi bi-heart-fill"></i>
   </span>
-  <ul class="flex gap-x-4 min-w-[320px] w-[100%] max-w-[420px] sm:justify-end">
-    <li><a href="" class="text-[#57C7FF]">Home</a></li>
-    <li><a href="" class="text-[#57C7FF]">Sobre</a></li>
-    <li><a href="" class="text-[#57C7FF]">Produtos</a></li>
-    <li><a href="" class="text-[#57C7FF]">Formulário</a></li>
+  <ul class="flex min-w-[320px] w-[100%] max-w-[420px] sm:justify-end gap-x-1">
+    <li><a href="" class="p-2 text-[#57C7FF] transition-colors duration-300 ease-in-out hover:bg-[#57C7FF] hover:text-[#0D0D0D] rounded-lg">Home</a></li>
+    <li><a href="" class="p-2 text-[#57C7FF] transition-colors duration-300 ease-in-out hover:bg-[#57C7FF] hover:text-[#0D0D0D] rounded-lg">Sobre</a></li>
+    <li><a href="" class="p-2 text-[#57C7FF] transition-colors duration-300 ease-in-out hover:bg-[#57C7FF] hover:text-[#0D0D0D] rounded-lg">Produtos</a></li>
+    <li><a href="" class="p-2 text-[#57C7FF] transition-colors duration-300 ease-in-out hover:bg-[#57C7FF] hover:text-[#0D0D0D] rounded-lg">Formulário</a></li>
   </ul>
   <?php ?>
 </header>
