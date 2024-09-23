69da821c5d61beea9b1fa8de2f0300cf5cb3ec34
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-09-23 15:09:45 -0300

    Current page highlighted

--- view/components/header.php
+++ view/components/header.php
@@ -4,10 +4,35 @@
     <i class="bi bi-heart-fill"></i>
   </span>
   <ul class="flex min-w-[320px] w-[100%] max-w-[420px] sm:justify-end gap-x-1">
-    <li><a href="" class="p-2 text-[#57C7FF] transition-colors duration-300 ease-in-out hover:bg-[#57C7FF] hover:text-[#0D0D0D] rounded-lg">Home</a></li>
-    <li><a href="" class="p-2 text-[#57C7FF] transition-colors duration-300 ease-in-out hover:bg-[#57C7FF] hover:text-[#0D0D0D] rounded-lg">Sobre</a></li>
-    <li><a href="" class="p-2 text-[#57C7FF] transition-colors duration-300 ease-in-out hover:bg-[#57C7FF] hover:text-[#0D0D0D] rounded-lg">Produtos</a></li>
-    <li><a href="" class="p-2 text-[#57C7FF] transition-colors duration-300 ease-in-out hover:bg-[#57C7FF] hover:text-[#0D0D0D] rounded-lg">Formulário</a></li>
+    <li><a href="" class="p-2 bg-inherit text-[#57C7FF] transition-colors duration-300 ease-in-out rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]">Home</a></li>
+    <li><a href="" class="p-2 bg-inherit text-[#57C7FF] transition-colors duration-300 ease-in-out rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]">Sobre</a></li>
+    <li><a href="" class="p-2 bg-inherit text-[#57C7FF] transition-colors duration-300 ease-in-out rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]">Produtos</a></li>
+    <li><a href="" class="p-2 bg-inherit text-[#57C7FF] transition-colors duration-300 ease-in-out rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]">Formulário</a></li>
   </ul>
+  <script>
+    const pathName = window.location.pathname;
+    const shortPathName = pathName.slice(15);
+    const anchors = document.querySelectorAll("a");
+
+    window.addEventListener("DOMContentLoaded", () => {
+      switch (shortPathName) {
+        case "":
+          anchors[0].classList.replace("bg-inherit", "bg-[#57C7FF]");
+          anchors[0].classList.replace("text-[#57C7FF]", "text-[#0D0D0D]");
+          break;
+        case "about.php":
+          anchors[1].classList.replace("bg-inherit", "bg-[#57C7FF]");
+          anchors[1].classList.replace("text-[#57C7FF]", "text-[#0D0D0D]");
+          break;
+        case "list.php":
+          anchors[2].classList.replace("bg-inherit", "bg-[#57C7FF]");
+          anchors[2].classList.replace("text-[#57C7FF]", "text-[#0D0D0D]");
+          break;
+        case "form.php":
+          anchors[3].classList.replace("bg-inherit", "bg-[#57C7FF]");
+          anchors[3].classList.replace("text-[#57C7FF]", "text-[#0D0D0D]");
+      }
+    });
+  </script>
   <?php ?>
 </header>
