8d7b5a8cc56cfb2cfecc0f87eda7348270fbc2a0
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-09-29 14:08:13 -0300

    made adjusts

--- view/components/footer.php
+++ view/components/footer.php
@@ -1,5 +1,15 @@
-<footer class="bg-[#0D0D0D] h-[80px] text-white p-4 flex flex-wrap items-center justify-center gap-3 sm:justify-between sm:px-8">
-  <p class="">&copy; 2024 Pet Shop &mdash; <a href="https://www.mit.edu/~amini/LICENSE.md" class="text-[#57C7FF]" target="_blank">Licença MIT</a></p>
+<footer
+  class="bg-[#0D0D0D] h-[80px] text-white p-4 flex flex-wrap items-center justify-center gap-3 sm:justify-between sm:px-8"
+>
+  <p class="">
+    &copy; 2024 Pet Shop &mdash;
+    <a
+      href="https://www.mit.edu/~amini/LICENSE.md"
+      class="text-[#57C7FF]"
+      target="_blank"
+      >Licença MIT</a
+    >
+  </p>
   <a href="https://github.com/zolppy/pet-shop" target="_blank">
     <i class="bi bi-github text-3xl"></i>
   </a>
--- view/components/header.php
+++ view/components/header.php
@@ -1,5 +1,6 @@
 <header
-  class="bg-[#0D0D0D] text-white p-4 h-[130px] flex flex-wrap gap-3 fixed top-0 left-0 w-full items-center sm:justify-between z-10 sm:px-8">
+  class="bg-[#0D0D0D] text-white p-4 h-[130px] flex flex-wrap gap-3 fixed top-0 left-0 w-full items-center sm:justify-between z-10 sm:px-8"
+>
   <span class="flex gap-x-2 text-3xl">
     <h1 class="font-bold">Pet Shop</h1>
     <i class="bi bi-heart-fill"></i>
@@ -7,23 +8,31 @@
   <ul class="flex sm:justify-end gap-x-2">
     <li>
       <a
-        href="/pet-shop/"
-        class="p-2 bg-inherit text-[#57C7FF] border border-[#57C7FF] transition-colors duration-200 rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]">Home</a>
+        href="/"
+        class="p-2 bg-inherit text-[#57C7FF] border border-[#57C7FF] transition-colors duration-200 rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]"
+        >Home</a
+      >
     </li>
     <li>
       <a
-        href="/pet-shop/view/about.php"
-        class="p-2 bg-inherit text-[#57C7FF] border border-[#57C7FF] transition-colors duration-200 rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]">Sobre</a>
+        href="/view/about.php"
+        class="p-2 bg-inherit text-[#57C7FF] border border-[#57C7FF] transition-colors duration-200 rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]"
+        >Sobre</a
+      >
     </li>
     <li>
       <a
-        href="/pet-shop/view/products.php"
-        class="p-2 bg-inherit text-[#57C7FF] border border-[#57C7FF] transition-colors duration-200 rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]">Produtos</a>
+        href="/view/products.php"
+        class="p-2 bg-inherit text-[#57C7FF] border border-[#57C7FF] transition-colors duration-200 rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]"
+        >Produtos</a
+      >
     </li>
     <li>
       <a
-        href="/pet-shop/view/contact-us.php"
-        class="p-2 bg-inherit text-[#57C7FF] border border-[#57C7FF] transition-colors duration-200 rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]">Fale Conosco</a>
+        href="/view/contact-us.php"
+        class="p-2 bg-inherit text-[#57C7FF] border border-[#57C7FF] transition-colors duration-200 rounded-lg hover:bg-[#57C7FF] hover:text-[#0D0D0D]"
+        >Fale Conosco</a
+      >
     </li>
   </ul>
   <script>
