4fe05e1a813ae4a00c1e6f077aa65f0aa9445095
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-09-30 01:10:28 -0300

    some adjusts

--- view/categories/index.js
+++ view/categories/index.js
@@ -1,24 +1,24 @@
-const editBtn = document.querySelector(".edit-btn");
-const editCategoriesBtn = document.querySelector(".categories button");
+const editCategorieBtns = document.querySelectorAll(".edit-btn");
 const closeBtn = document.querySelector("dialog header button");
-const editProductBtns = document.querySelectorAll(".edit-product");
 
 function openModal() {
   const modal = document.querySelector("dialog");
   modal.showModal();
 }
 
-editBtn.addEventListener("click", (event) => {
-  const modal = document.querySelector("dialog");
-  const modalName = modal.querySelector("#name");
-  const el = event.target.closest("li");
+for (const btn of editCategorieBtns) {
+  btn.addEventListener("click", (event) => {
+    const modal = document.querySelector("dialog");
+    const modalName = modal.querySelector("#name");
+    const el = event.target.closest("li");
 
-  const name = el.querySelector(".category-name").textContent;
+    const name = el.querySelector(".category-name").textContent;
 
-  modalName.value = name;
+    modalName.value = name.trim();
 
-  openModal();
-});
+    openModal();
+  });
+}
 
 closeBtn.addEventListener("click", () => {
   const modal = document.querySelector("dialog");
